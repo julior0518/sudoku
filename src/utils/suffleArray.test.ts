f65798febf0f2c7fa232517f6f529ca620ed8417
julior0518 <julior0518@gmail.com>
2022-06-06 16:24:32 -0400

    wrote test for shuffle algorythim

--- src/utils/suffleArray.test.ts
+++ src/utils/suffleArray.test.ts
@@ -1,10 +1,19 @@
 import { Shuffle } from ".";
 
-describe('',()=> {
-    it('',()=>{
+describe('Shuffle',()=> {
+    it('check lenght of array after shuffle',()=>{
+        const a = [1,2,3]
+        Shuffle(a)
+        expect(a).toHaveLength(3)
 
     })
-    it('',()=>{
-        
+    it('check if array contains same values',()=>{
+        const a = [1,2,3]
+        Shuffle(a)
+        expect(a).toContain(1)
+        expect(a).toContain(2)
+        expect(a).toContain(3)
+        console.log('example shuffle', a)
     })
+
 })
