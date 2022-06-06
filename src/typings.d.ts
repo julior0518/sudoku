f7e010a77339be6387f50830c6477a30b34909d8
julior0518 <julior0518@gmail.com>
2022-06-06 14:54:41 -0400

    added shuffle

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,9 +1,9 @@
 export type ThemeType = typeof theme; 
 
 
-export type GRID = [][]
 
-export type N = 0 | 1| 2 | 3 | 4 | 5 | 7 | 8 | 9 
-export type NUMBERS = 0 | 1| 2 | 3 | 4 | 5 | 7 | 8 | 9 
+export type N = 0 | 1| 2 | 3 | 4 | 5 | 6|  7 | 8 | 9 
+export type NUMBERS = 0 | 1| 2 | 3 | 4 | 5 | 6|  7 | 8 | 9 
 
+export type GRID = [ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW, ROW]
 export type ROW = [N, N, N, N, N, N, N, N, N]
--- src/utils/grid_fillGrid.ts
+++ src/utils/grid_fillGrid.ts
@@ -1,3 +1,5 @@
+import {GRID, NUMBERS} from '../typings'
+
 // const grid = [
 //     [0,0,0,0,0,0,0,0,0],
 //     [0,0,0,0,0,0,0,0,0],
@@ -10,9 +12,24 @@
 //     [0,0,0,0,0,0,0,0,0]
 // ]
 
-function fillgrid(grid: any) {
+const initialNumbers: NUMBERS[] = [1,2,3,4,5,6,7,8,9]
 
+function fillgrid(grid: GRID) {
+
+    let row = 0;
+    let col = 0;
+
+    for (let i = 0; i < 81; i++) {
+        row = Math.floor(i / 9);
+        col = i % 9
+
+        if (grid[row][col] === 0) {
+            
+
+        }
+    }
     
+
 }
 
 export default fillgrid;
